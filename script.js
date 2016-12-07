0fe5cda3782bbb4b96c199dc1c1e3692ae71981e
Lupo-Brittaney <lup13002@byui.edu>
2016-12-07 09:39:40 -0700

    xml
    xmltesting

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 //All javascript logic for the app
-	var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function(){
-			if (this.readyState == 4 && this.status == 200){
-				var allergenNames = JSON.parse(xhr.responseText);
-				document.getElementById("testArea").innerHTML = "ran";
-			}
-		
-		};	
-		xhr.open('GET', 'allergenList.json');
-		xhr.send();
+
 
 
 
--- xhrequest.js
+++ xhrequest.js
@@ -1,9 +1,11 @@
 //This is where the arrays that are saved in JSON files are requested and returned to the app
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-	if (xhr.readyState === 4){
-		var dairyNames = JSON.parse(xhr.responseText);
-		}	
-};
-xhr.open('GET', 'dairy.json');
-xhr.send();
+	var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(){
+			if (this.readyState == 4 && this.status == 200){
+				var allergenNames = JSON.parse(xhr.responseText);
+				document.getElementById("testArea").innerHTML = "ran";
+			}
+		
+		};	
+		xhr.open('GET', 'allergenList.json');
+		xhr.send();
