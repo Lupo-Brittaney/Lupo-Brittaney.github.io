23b15086bf6d38b37f44b7b7360d0b8c70c838c8
Lupo-Brittaney <lup13002@byui.edu>
2016-12-07 09:46:09 -0700

    xml
    xml test

--- xhrequest.js
+++ xhrequest.js
@@ -1,11 +1,10 @@
 //This is where the arrays that are saved in JSON files are requested and returned to the app
 	var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function(){
-			if (this.readyState == 4 && this.status == 200){
-				var allergenNames = JSON.parse(xhr.responseText);
-				document.getElementById("testArea").innerHTML = "ran";
-			}
-		
+	xhr.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200){
+			var allergenNames = JSON.parse(xhr.responseText);
+			document.getElementById('testArea').innerHTML = "ran";
+			}	
 		};	
-		xhr.open('GET', 'allergenList.json');
-		xhr.send();
+	xhr.open('GET', 'allergenList.json');
+	xhr.send();
