930913f3fa4a8f6bd5683b48dbec226e25c5f4d5
Lupo-Brittaney <lup13002@byui.edu>
2016-12-10 18:44:58 -0700

    Delete xhrgluten.js
    no longer needed

--- xhrgluten.js
+++
@@ -1,9 +0,0 @@
-//This is where the arrays that are saved in JSON files are requested and returned to the app
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-	if (xhr.readyState === 4){
-		var glutenNames = JSON.parse(xhr.responseText);
-		}
-};
-xhr.open('GET', 'gluten.json');
-xhr.send();
